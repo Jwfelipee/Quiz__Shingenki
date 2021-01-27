6438d108c47cdcdbfe8186b64adc00c73d417bea
Jwfelipee <arthur993897437@gmail.com>
2021-01-27 18:48:08 -0300

    criado a pagina de perguntas, adicionado as input das respostas, iniciada a tela de loading

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 /*
 const BackgroundImage = styled.div`
@@ -14,30 +22,43 @@ const BackgroundImage = styled.div`
 `;
 */
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 69%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
-
-
 export default function Home() {
+  const router = useRouter()
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        Shingenki Quiz 
+      </Head>
       <QuizContainer>
+      <QuizLogo />
        <Widget>
            <Widget.Header>
            <h1>Shingenki no Kyojin</h1>
            </Widget.Header>
          <Widget.Content>
-         
-         <p>sasageyo...</p>
+            <form onSubmit={function (infosDoEvento){
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log("fazendo uma submissão por meio do react");
+
+
+              //router manda para a próxima pagina
+            }}>
+              <Input // State estado do componente.
+              name="nomeDoUsuario"
+              onChange={function (infosDoEvento){
+                console.log(infosDoEvento.target.value);
+                setName(infosDoEvento.target.value);
+              }}
+              placeholder="escreva seu nome!" 
+              value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                quem vai jogar é {name}
+              </Button>
+            </form>
          </Widget.Content>
        </Widget>
 
